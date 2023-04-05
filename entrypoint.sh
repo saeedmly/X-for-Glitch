91a8ec3d2188a4c6c3d28523a58a086c163d5966
saeedmly <129755232+saeedmly@users.noreply.github.com>
2023-04-05 15:48:20 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'085fef3e-6ab3-4715-a28c-ff41cba7060a'}
 
 # 哪吒三个参数，不需要的话可以留空，删除或在这三行最前面加 # 以注释
 NEZHA_SERVER= 
